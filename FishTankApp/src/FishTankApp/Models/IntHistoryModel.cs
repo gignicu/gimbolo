2255d9b8e2da8ca09ecb20512196741774c20775
gignicu <gig_nicu@yahoo.com>
2016-01-20 18:35:18 +0200

    First commit FishApp

---
+++ FishTankApp/src/FishTankApp/Models/IntHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTankApp.Models
+{
+    public class IntHistoryModel
+    {
+        public IntHistoryModel(DateTime timeStamp, int value)
+        {
+            TimeStamp = timeStamp;
+            Value = value;
+        }
+
+        public DateTime TimeStamp { get; private set; }
+        public int Value { get; private set; }
+    }
+}
